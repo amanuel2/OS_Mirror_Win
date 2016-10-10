613982a15943c8ee27a8df8de7d57c8a4daaa5c0
Amanual Bogale <abogal6274@inst.hcpss.org>
2016-10-10 07:34:46 -0400

    Changes

--- kernel.asm
+++ kernel.asm
@@ -1,10 +1,13 @@
 ;ORG 0x9000
 BITS 32
 
+;extern kernel_main_c
+
 k_main:
 	mov byte [0xB8000], 88
 	mov byte [0xB8000+1], 0x1B
-
+		
+;		call kernel_main_c
 
 	mov byte [0xB8000+4], 89
 	mov byte [0xB8000+5], 0x1B
